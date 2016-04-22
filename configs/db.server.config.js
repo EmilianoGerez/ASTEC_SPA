6dd75917c3a5b602f7cd59cae3fb8bd79c740131
Emiliano Gerez <Emiliano Gerez>
2016-04-22 00:35:25 -0300

    change a new db

--- configs/db.server.config.js
+++ configs/db.server.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://heroku_4n39tnlx:dev1988@ds013891.mongolab.com:13891/heroku_4n39tnlx'
+    url: 'mongodb://adminDB:admin1234@ds013891.mlab.com:13891/dbfortest'
 };

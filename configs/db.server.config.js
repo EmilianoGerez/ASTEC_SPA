f1f25e58a004c7e9ccce6ef6968b725eb9919cbc
Emiliano Gerez <Emiliano Gerez>
2016-04-22 00:14:28 -0300

    change in uri db

--- configs/db.server.config.js
+++ configs/db.server.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://heroku_4n39tnlx:dev1988@ds013891.mongolab.com:45694/heroku_4n39tnlx'
+    url: 'mongodb://heroku_4n39tnlx:dev1988@ds013891.mongolab.com:13891/heroku_4n39tnlx'
 };

896cf631897dfb12a78dc2f006d405b6519e42bd
Emi Gerez <Emi Gerez>
2016-05-13 02:05:20 -0300

    ignore

--- configs/token.server.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    SECRET_KEY: 'yoursecretkey',
-    EXP_TIME: 1800, // 1 min, in real app can use 15 min
-    SESSION_TIME: 604800
-};
--- configs/user-code.server.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	ADMIN_CODE: 'yb9b637i2v',
-	TECH_CODE: 'dw77yee18u'
-};
